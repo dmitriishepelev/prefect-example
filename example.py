3796ba3b3ad56aba565df4556fe1066a7cda8b91
dmitriishepelev <dshepelev@artisight.com>
2025-01-28 19:57:00 +0400

    Add subflow

--- example.py
+++ example.py
@@ -12,9 +12,16 @@ def show_variable(key):
     print("done")
 
 
+@flow(name="Subflow")
+def run_subflow(key):
+    print(key, "=>", os.getenv(key))
+    time.sleep(randint(1, 3))
+
+
 @flow(log_prints=True)
 def show_env_variables():
     for i, key in enumerate(os.environ.keys(), start=1):
         print(i, key)
         show_variable(key)
+        run_subflow(key)
         print("wait for previous tasks")
