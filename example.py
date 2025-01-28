449c16a751e709b6044aed1a8c279d2e1f474899
dmitriishepelev <dshepelev@artisight.com>
2025-01-28 19:47:47 +0400

    Add flow example

---
+++ example.py
@@ -0,0 +1,20 @@
+import os
+import time
+from random import randint
+
+from prefect import flow, task
+
+
+@task(log_prints=True)
+def show_variable(key):
+    print(key, "=>", os.getenv(key))
+    time.sleep(randint(5, 10))
+    print("done")
+
+
+@flow(log_prints=True)
+def show_env_variables():
+    for i, key in enumerate(os.environ.keys(), start=1):
+        print(i, key)
+        show_variable.apply_async(key)
+        print("wait for previous tasks")
